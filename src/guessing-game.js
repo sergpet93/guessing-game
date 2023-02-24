79f3f9f99d3d057793b7972aff9f064dc2b82b0b
sergpet93 <maknev96@mail.ru>
2023-02-24 11:24:51 +0300

    docs: update guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.value = 0
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        this.value = Math.round((this.max + this.min) / 2)
+        return this.value
     }
 
     lower() {
-
+        this.max = this.value;
     }
 
     greater() {
-
+        this.min = this.value;
     }
 }
 
